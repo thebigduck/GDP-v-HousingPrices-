b5a1bbd92fabe2367d8afc32a06b157c57e58e74
thebigduck <59844955+thebigduck@users.noreply.github.com>
2024-06-07 18:45:31 -0400

    viewport fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,8 @@ export default function RootLayout({
   return (
     <html lang="en">
           <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
     </Head>
       <body>{children}</body>
     </html>
--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -15,7 +15,8 @@ export default function Document() {
             }
           `}
         </Script>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
         <Main />
